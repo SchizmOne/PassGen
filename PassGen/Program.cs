6c07a424aae7ba777d9db22920d08d2d3b8958b2
SchizmOne <schizm.one@gmail.com>
2019-10-05 12:15:54 +0700

    Тест заливки.

---
+++ PassGen/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassGen
+{
+    enum CharactersSet
+    {
+        nonSymbols,
+        lowerCaseSymbols,
+        biggerCaseSymbols,
+        specialSymbols,
+        numbersSymbols
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length != 0)
+            {
+
+                CharactersSet[] characterOption = new CharactersSet[4];
+                if (args.Contains("-A")) characterOption[0] = CharactersSet.lowerCaseSymbols;
+                if (args.Contains("-c")) characterOption[1] = CharactersSet.biggerCaseSymbols;
+                if (args.Contains("-n")) characterOption[2] = CharactersSet.numbersSymbols;
+                if (args.Contains("-l")) characterOption[3] = CharactersSet.specialSymbols;
+
+                int passwordLength = 0;
+                int numberOfPasswords = 0;
+                foreach (string arg in args)
+                {
+                    if (int.TryParse(arg, out int argIntPasswordLength) && passwordLength == 0)
+                        passwordLength = argIntPasswordLength;
+                    if (int.TryParse(arg, out int argIntNumberOfPasswords) && numberOfPasswords == 0)
+                        numberOfPasswords = argIntNumberOfPasswords;
+                }
+
+                Console.WriteLine("Passwords:");
+                Console.WriteLine(characterOption[0].ToString() + characterOption[1].ToString() + characterOption[2].ToString() + characterOption[3].ToString());
+
+                for (int i = 0; i < numberOfPasswords; i++)
+                {
+                    Console.WriteLine(StringGenerator.ChooseCharactersSet(passwordLength, characterOption));
+                }
+
+            }
+
+        }
+    }
+}
---
+++ PassGen/StringGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace PassGen
+{
+    class StringGenerator
+    {
+
+        public static string ChooseCharactersSet(int passwordLength, params CharactersSet[] charactersOption)
+        {
+            const string lowerCaseSymbols =  "abcdefghijklmnopqrstuvwxyz",
+                         biggerCaseSymbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                         specialSymbols =    "!@#$%^&*()_-+=`~?';<>{}[]|\\",
+                         numbersSymbols =    "1234567890";
+
+            string resultCharacterSet = "";
+            if (charactersOption.Contains(CharactersSet.lowerCaseSymbols))
+                resultCharacterSet += lowerCaseSymbols;
+            if (charactersOption.Contains(CharactersSet.biggerCaseSymbols))
+                resultCharacterSet += biggerCaseSymbols;
+            if (charactersOption.Contains(CharactersSet.specialSymbols))
+                resultCharacterSet += specialSymbols;
+            if (charactersOption.Contains(CharactersSet.numbersSymbols))
+                resultCharacterSet += numbersSymbols;
+
+            return GenerateString(passwordLength, resultCharacterSet);
+        }
+
+        public static string GenerateString(int length, string characterSet)
+        {
+            byte[] bytes = new byte[length * 8];
+            char[] result = new char[length];
+            var characterArray = characterSet.Distinct().ToArray();
+
+            using (var cryptoProvider = new RNGCryptoServiceProvider())
+            {
+                cryptoProvider.GetBytes(bytes);
+            }
+            for (int i = 0; i < length; i++)
+            {
+                ulong value = BitConverter.ToUInt64(bytes, i * 8);
+                result[i] = characterArray[value % (uint)characterArray.Length];
+            }
+            return new string(result);
+        }
+    }
+}
